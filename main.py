e8011108f915f1fce572b54cc14e2536b303c0be
Micha <177941509+2mjlux@users.noreply.github.com>
2025-08-02 17:54:51 +0200

    F: add reporting functionality

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from stats import count_words, count_characters
+import sys
+from stats import count_words, count_characters, sort_on, sort_list_dict
 
 def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		return f.read()
 
 def main():
-	num_words = count_words(get_book_text(path_to_file))
-	print(f"{num_words} words found in the document")
-	print(count_characters(get_book_text(path_to_file)))
+    
+    if len(sys.argv)!=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    path_to_file = sys.argv[1]
+	
+	# pass data as parameters
+    book_text = get_book_text(path_to_file)
+    num_words = count_words(book_text)
+    char_count = count_characters(book_text)
+    sorted_char_count = sort_list_dict(char_count)
+    
+    print_report(path_to_file, num_words, sorted_char_count)
 
-path_to_file = "books/frankenstein.txt"
+def print_report(file_path, word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_data in sorted_chars:
+        if char_data["char"].isalpha():
+            print(f"{char_data['char']}: {char_data['num']}")
+    print("============= END ===============")
+
+# path_to_file = "books/frankenstein.txt"
 
 main()
--- stats.py
+++ stats.py
@@ -10,4 +10,14 @@ def count_characters(text):
 		else:
 			char_count[char] = 1
 	return char_count
+	
+def sort_on(items):
+    return items["num"]	
 		
+def sort_list_dict(char_dict):
+	list_char_dict = []
+	for char, num in char_dict.items():
+		list_char_dict.append({"char": char, "num": num})
+	list_char_dict.sort(reverse=True, key=sort_on)
+	return list_char_dict
+
